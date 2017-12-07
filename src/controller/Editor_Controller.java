5decf68f71cfa1ff1b0f68fb456113287b67fd1f
charamza <charamza@UCEBNA-PC-M-14.gjkt.local>
2017-12-07 14:55:16 +0100

    wall

--- src/controller/Editor_Controller.java
+++ src/controller/Editor_Controller.java
@@ -116,6 +116,19 @@ public class Editor_Controller implements Initializable{
 
 		drawCanvas();
 		
+		for (int x = 0; x < 20; x++){
+			for (int y = 0; y < 20; y++){
+				if (x == 0 && y == 0){	
+					Editor_Object ObjWall = new Editor_Object(x,y,gh.getWall(),"Wall");
+					allEditor_Objects.add(ObjWall);
+				}
+				if (x == 20 && y == 20){
+					Editor_Object ObjWall = new Editor_Object(x,y,gh.getWall(),"Wall");
+					allEditor_Objects.add(ObjWall);
+				}
+			}
+		}
+		
 	} 
 	
 	public void setKeyboardInput() {
