740efc11f66fbce325fe93e95891ac2af59794c3
PavR <pav.rezny@seznam.cz>
2017-12-11 21:34:15 +0100

    s

--- src/controller/Main_Controller.java
+++ src/controller/Main_Controller.java
@@ -20,7 +20,6 @@ import handler.Sound_Handler;
 import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Cursor;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
